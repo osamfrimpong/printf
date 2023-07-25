b9a30234d08db2a3a3deefd29ee719365c764cf6
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-07-25 16:14:28 +0000

    Modified _print_f.c

--- main.h
+++ main.h
@@ -39,7 +39,7 @@ struct fmt
  */
 typedef struct fmt fmt_t;
 
-void print_buffer(char buffer[], int *buff_ind);
+
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
